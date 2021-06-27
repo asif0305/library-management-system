026e931953f26b4ac3fcdef99014aa549d2f3f51
Mohammad Asif <66441922+asif0305@users.noreply.github.com>
2021-06-27 23:02:31 +0530

    Add files via upload

---
+++ src/header.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
+		<link rel="stylesheet" type="text/css" href="css/header_style.css" />
+		
+	</head>
+	<body>
+		<header>
+			<a href="../index.php">
+				<div id="cd-logo">
+					<img src="img/ic_logo.svg" style="margin: 10px;" alt="Logo" />
+					<p>LIBRARY</p>
+					<p style="font-size:10px;padding-left:12px">Integral University</p>
+					
+				</div>
+			</a>
+		</header>
+	</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,44 @@
+<?php
+require "db_connect.php";
+require "header.php";
+session_start();
+
+if (empty($_SESSION['type']));
+else if (strcmp($_SESSION['type'], "librarian") == 0)
+	header("Location: librarian/home.php");
+else if (strcmp($_SESSION['type'], "member") == 0)
+	header("Location: member/home.php");
+?>
+
+<html>
+
+<head>
+	<title>Library Management</title>
+	<link rel="stylesheet" type="text/css" href="css/index_style.css" />
+</head>
+
+<body>
+	<div id="allTheThings" >
+		<div id="member">
+			<a href="member">
+				<img src="img/ic_member.svg" width="250px" height="auto" /><br />
+				&nbsp;Member
+			</a>
+		</div>
+		<div id="verticalLine">
+			<div id="librarian">
+				<a id="librarian-link" href="librarian">
+					<img src="img/ic_librarian.svg" width="250px" height="auto" /><br />
+					&nbsp;&nbsp;&nbsp;Librarian
+				</a>
+			</div>
+		</div>
+	</div>
+	<footer style="text-align: center;">
+		<div>
+			<a> Integral University Library Managment System<p>© 2021 Copyright</p></a>
+		</div>
+	</footer>
+</body>
+
+</html>
